34ca6e9715fd2f800c0381ef0d91d19b315dc838
Samreaksa Ros <15318223+monkeyidesign@users.noreply.github.com>
2019-04-11 11:48:45 +0700

    Chaning internal to external script

---
+++ alert.js
@@ -0,0 +1,9 @@
+//Author: Samreaksa Ros
+//Mobile: +85592 200069
+//https://www.monkeyidesign.com
+
+function fryRice(){
+  var name = "Get data from Phu input";//static data
+  var names = document.getElementById("text1").value;//gat data from user input
+  alert("Phu homework:" + names);
+}
